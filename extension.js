d297d9af091b54a98b73f84ddc622cce52d08923
Tripp Lyons <tripplyons@gmail.com>
2024-08-14 13:56:54 -0700

    add support for notebook editors

--- extension.js
+++ extension.js
@@ -4,13 +4,25 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	function pinActiveEditor() {
+		vscode.commands.executeCommand('moveActiveEditor', { to: 'first' });
+	}
+
 	let disposable = vscode.window.onDidChangeActiveTextEditor((editor) => {
 		if (editor) {
-			vscode.commands.executeCommand('moveActiveEditor', { to: 'first' });
+			pinActiveEditor();
 		}
 	});
 
 	context.subscriptions.push(disposable);
+
+	let disposable2 = vscode.window.onDidChangeActiveNotebookEditor((editor) => {
+		if (editor) {
+			pinActiveEditor();
+		}
+	});
+
+	context.subscriptions.push(disposable2);
 }
 
 function deactivate() {}
